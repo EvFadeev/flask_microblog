f1800c8bdaea47fe74a0ffbc6a11ea88dcd0f6b8
EvFadeev <mercenary.exe@gmail.com>
2021-11-18 19:20:42 +0300

    added configuration for app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+
+# добавление конфигурации
+""" app.config['SECRET_KEY'] = 'you-will-never-guess' """
+# ... add more variables here as needed
+
+# читаем и применяем файл конфигурации
+app.config.from_object(Config)
 
 from app import routes
