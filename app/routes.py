45bbd78d6ebeffe45634794e60e4fe9ed6ee29ff
EvFadeev <mercenary.exe@gmail.com>
2021-11-19 04:22:30 +0300

    gettind data from form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask.typing import TemplateFilterCallable
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/')
@@ -22,9 +22,17 @@ def index():
     ]
     return render_template('index.html', user=user, title='Home', posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # получение данных формы
+    if form.validate_on_submit():
+        # flash() — полезный способ показать сообщение пользователю
+        # Многие приложения используют эту технику, 
+        # чтобы сообщить пользователю, было ли какое-либо действие успешным или нет
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
 
 
