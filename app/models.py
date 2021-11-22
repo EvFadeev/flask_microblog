9aeb71d4d426b6a43377c1eaeabbea61de346a69
EvFadeev <mercenary.exe@gmail.com>
2021-11-22 18:33:26 +0300

    connection between db user and db post

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from enum import unique
-from operator import index
+from sqlalchemy.orm import backref
 from app import db
+from datetime import datetime
 
 # база данных пользователя
 class User(db.Model):
@@ -9,7 +9,18 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-
+    # представление о взаимоотношении между user и post ("один ко многим")
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     # сообщает как печатать объекты данного класса (полезно при отладке)
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# база данных постов пользователей
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
