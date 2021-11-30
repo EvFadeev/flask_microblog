e307706ba75fe7ef7464a687a0007d3208148ef1
EvFadeev <mercenary.exe@gmail.com>
2021-11-30 04:17:03 +0300

    added user loader

--- app/models.py
+++ app/models.py
@@ -1,18 +1,23 @@
 from sqlalchemy.orm import backref
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from app import login
 # хеширование и проверка пароля
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # база данных пользователя
-class User(db.Model):
+class User(UserMixin, db.Model):
+
     # создание полей базы данных
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
     # представление о взаимоотношении между user и post ("один ко многим")
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     # сообщает как печатать объекты данного класса (полезно при отладке)
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -32,3 +37,11 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+# Поскольку Flask-Login ничего не знает о базах данных, 
+# ему нужна помощь приложения при загрузке пользователя. 
+# По этой причине расширение ожидает, что приложение настроит функцию загрузчика пользователя, 
+# которую можно вызвать для загрузки пользователя с идентификатором.
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
